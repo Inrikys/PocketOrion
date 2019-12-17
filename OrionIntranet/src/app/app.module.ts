4c653ad92cd632ab3d710f26170412ee26d204af
Henrique Sousa <inrikys@gmail.com>
2019-12-16 21:54:39 -0300

    ajustes iniciais

---
+++ OrionIntranet/src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { environment } from '../environments/environment';
+
+// BrMasker
+import { BrMaskerModule } from 'br-mask';
+
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
+
+// Font Awesome
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
+import {fab} from '@fortawesome/free-brands-svg-icons';
+library.add(fas, far, fab);
+
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    BrMaskerModule,
+    FontAwesomeModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    AngularFireAuth,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ OrionIntranet/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDG7nJWfa7kUQDVLCpwDPZUwPHMZzEZFho',
+    authDomain: 'pocketorion.firebaseapp.com',
+    databaseURL: 'https://pocketorion.firebaseio.com/',
+    projectId: 'pocketorion',
+    storageBucket: 'pocketorion.appspot.com',
+  },
+};
